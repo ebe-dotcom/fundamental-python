483fed2273b56fcdc1dbe21c0c0bd76e69e6bd76
Eka Bernard <bernard.eka@gmail.com>
2021-02-14 00:52:16 +0700

    Data percabangan

---
+++ start.py
@@ -0,0 +1,15 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL : Eksekusi Berurutan
+
+
+print('Hello World!')
+print('by. Ebe')
+print('Tanggal 14 Februari 2021')
+print('--' * 10)
+
+# PERCABANGAN : Eksekusi Terpilih
+Ingin_cepat = True
+if ingin_cepat:
+    print('Ambil jalan memutar')
+else:
+    print('Jalan lurus saja ya')
