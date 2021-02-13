9b197b564829da77ea1548d4cd0a6f150635e89e
Eka Bernard <bernard.eka@gmail.com>
2021-02-14 01:07:50 +0700

    Data percabangan

--- start.py
+++ start.py
@@ -8,8 +8,19 @@ print('Tanggal 14 Februari 2021')
 print('--' * 10)
 
 # PERCABANGAN : Eksekusi Terpilih
-Ingin_cepat = True
+ingin_cepat = False
 if ingin_cepat:
     print('Ambil jalan memutar')
 else:
     print('Jalan lurus saja ya')
+
+
+#PERULANGAN
+jumlah_anak = 4
+print('Halo Anak ke-1')
+print('Halo Anak ke-2')
+print('Halo Anak ke-3')
+print('Halo Anak ke-4')
+#atau
+for index_anak in range(1,jumlah_anak+1):#jumlah perulangan 5 - 1 = 4
+    print(f'Halo Anak ke-{index_anak}')
